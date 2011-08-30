c7bcdada598399eea55ca9fa1af0fbd14b93b45b
James Morgan <james.morgan.e@googlemail.com>
2011-08-30 21:59:26 +0100

    Added Primary Key to organisations and fixe unique key

--- src/main/sql/organisation.sql
+++ src/main/sql/organisation.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `organisations` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `OrganisationKey` int(10) unsigned NOT NULL DEFAULT '0',
   `PostcodeType` varchar(1) NOT NULL DEFAULT '',
   `OrganisationName` varchar(60) NOT NULL DEFAULT '',
   `DepartmentName` varchar(60) NOT NULL DEFAULT '',
-  PRIMARY KEY (`OrganisationKey`)
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY (`OrganisationKey`, `PostcodeType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
