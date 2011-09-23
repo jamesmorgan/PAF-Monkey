45bdedae7ac54a9283bb1b087137f233a3e17960
james.morgan <james.morgan@leadx.com>
2011-09-23 15:28:42 +0100

    Removed ID from organisation

--- src/main/sql/organisation.sql
+++ src/main/sql/organisation.sql
@@ -1,10 +1,8 @@
 DROP TABLE IF EXISTS `organisations`;
 CREATE TABLE `organisations` (
-  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `OrganisationKey` int(10) unsigned NOT NULL DEFAULT '0',
   `PostcodeType` varchar(1) NOT NULL DEFAULT '',
   `OrganisationName` varchar(60) NOT NULL DEFAULT '',
   `DepartmentName` varchar(60) NOT NULL DEFAULT '',
-  PRIMARY KEY (`ID`),
   UNIQUE KEY (`OrganisationKey`, `PostcodeType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
